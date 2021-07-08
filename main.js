96d58af4635cb50d54979c55555b54110caff999
Candy Crush <76581343+Aadya-ai@users.noreply.github.com>
2021-07-08 14:36:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,10 +12,33 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+}
+
+function play() {
+    song_1.play();
+    song_1.setVolume();
+    song_1.rate();
 }
 
 function Click_here() {
